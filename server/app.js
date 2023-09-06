3c3b902627ca2e98628632f73a9c274815a8a5f8
spano <spano@student.chalmers.se>
2023-09-06 11:07:37 +0200

    Update base `app.js`
    This commit makes some minor changes to the `app.js` file of the
    back-end part of the project.

--- server/app.js
+++ server/app.js
@@ -1,16 +1,18 @@
-var express = require('express');
-var mongoose = require('mongoose');
-var morgan = require('morgan');
-var path = require('path');
-var cors = require('cors');
-var history = require('connect-history-api-fallback');
+// app.js - back-end (server) 
 
-// Variables
-var mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/animalDevelopmentDB';
-var port = process.env.PORT || 3000;
+const express   = require('express');
+const mongoose  = require('mongoose');
+const morgan    = require('morgan');
+const path      = require('path');
+const cors      = require('cors');
+const history   = require('connect-history-api-fallback');
+
+// Define variables
+const port      = process.env.PORT || 3000;
+const mongoURI  = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/animalDevelopmentDB';
 
 // Connect to MongoDB
-mongoose.connect(mongoURI).catch(function(err) {
+mongoose.connect(mongoURI).catch((err) => {
     if (err) {
         console.error(`Failed to connect to MongoDB with URI: ${mongoURI}`);
         console.error(err.stack);
@@ -20,40 +22,46 @@ mongoose.connect(mongoURI).catch(function(err) {
 });
 
 // Create Express app
-var app = express();
+const app = express();
+
 // Parse requests of content-type 'application/json'
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // HTTP request logger
 app.use(morgan('dev'));
+
 // Enable cross-origin resource sharing for frontend must be registered before api
 app.options('*', cors());
 app.use(cors());
 
-// Import routes
-app.get('/api', function(req, res) {
+// A test `api` route
+app.get('/api', (req, res) => {
     res.json({'message': 'Welcome to your DIT342 backend ExpressJS project!'});
 });
 
 // Catch all non-error handler for api (i.e., 404 Not Found)
-app.use('/api/*', function (req, res) {
+app.use('/api/*', (req, res) => {
     res.status(404).json({ 'message': 'Not Found' });
 });
 
 // Configuration for serving frontend in production mode
-// Support Vuejs HTML 5 history mode
+// Support Vue.js HTML 5 history mode
 app.use(history());
+
 // Serve static assets
-var root = path.normalize(__dirname + '/..');
-var client = path.join(root, 'client', 'dist');
+const root      = path.normalize(__dirname + '/..');
+const client    = path.join(root, 'client', 'dist');
+
 app.use(express.static(client));
 
 // Error handler (i.e., when exception is thrown) must be registered last
-var env = app.get('env');
+const env = app.get('env');
+
 // eslint-disable-next-line no-unused-vars
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
     console.error(err.stack);
-    var err_res = {
+    let err_res = {
         'message': err.message,
         'error': {}
     };
@@ -65,7 +73,8 @@ app.use(function(err, req, res, next) {
     res.json(err_res);
 });
 
-app.listen(port, function(err) {
+// listen on the defined port
+app.listen(port, (err) => {
     if (err) throw err;
     console.log(`Express server listening on port ${port}, in ${env} mode`);
     console.log(`Backend: http://localhost:${port}/api/`);
