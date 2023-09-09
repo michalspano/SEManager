aec1aeb7773fad75b89dc4ca36b9339ff2cb748c
spano <spano@student.chalmers.se>
2023-09-09 14:40:52 +0200

    #1 Add `Mongoose` model for `Course`
    This commit adds a Mongoose model for `Course` to the `server/models` directory. The model is defined in `server/models/course.js`.

---
+++ server/models/course.js
@@ -0,0 +1,28 @@
+// File: ./models/course.js
+// A mongoose model for a Course
+
+const mongoose  = require("mongoose");
+const Schema    = mongoose.Schema;
+
+const CourseModel = new Schema({
+    courseCode: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    courseName: {
+        type: String,
+        required: true
+    },
+    courseStaff: [{
+        type: String, // unique email
+        trim: true,
+        required: true
+    }],
+    dependencies: [{
+        type: String, // unique courseCode
+        trim: true
+    }]
+});
+
+module.exports = mongoose.model("Course", CourseModel);
