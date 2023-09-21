fdafb3d7058b77e804805d4bbd054ca4c947f142
spano <spano@student.chalmers.se>
2023-09-21 22:49:59 +0200

    #5 Update `config.js`
    This commit adds remaining `JSDoc` comments to the file and makes the
    `CONFIG` Object immutable (with the `Object.freeze` method).

--- server/controllers/v1/config.js
+++ server/controllers/v1/config.js
@@ -6,16 +6,16 @@
  */
 
 /**
- * @description A configuration {Object} for the API.
+ * @description An immutable configuration {Object} for the API.
  */
-const CONFIG = {
+const CONFIG = Object.freeze({
     "version": 'v1',
     "api_info": {
         "releaseDate": Date('2023-09-16'),
         "description": "Version 1 of the API for the SEM Program System.",
         "authors": ["Michal Spano", "Erik Lindstrand", "Ionel Pop Jara"]
     }
-};
+});
 
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || "http://localhost";
@@ -23,8 +23,9 @@ const HOST = process.env.HOST || "http://localhost";
 /**
  * This function formats the resource's URI for the purpose
  * of the HATEOAS mechanism.
+ * @param {String} resource - the type of the resource.
  * @param {String} id - the identifier of the resource.
- * @returns 
+ * @returns {String} - the formatted URI.
  */
 const formatHref = (resource, id) => {
     return `${HOST}:${PORT}/${CONFIG.version}/${resource}/${id}`;
