1f4f24670c09fee25efa3c844bd8bae2bcc5604a
Ionel Pop <ionelalexandru.pop@gmail.com>
2023-09-26 00:11:20 +0200

    #9 Add Course.vue
    Creates Course component

--- client/src/components/Course.vue
+++ client/src/components/Course.vue
@@ -1,57 +1,52 @@
 <script setup>
-    import { Course } from '@/Api';
-    import { ref } from 'vue';
-
-    defineProps(['title']);
-
-    const courseCode = ref('none');
-    const courseName = ref('none');
-    const courseStaff = ref('none');
-
-    const getCourse = () => {
-        Course.get('/').then((response) => {
-            console.log(response.data.course.courseCode);
-            courseCode.value = response.data.course.courseCode;
-            courseName.value = response.data.course.courseName;
-            courseStaff.value = response.data.course.courseStaff;
-        })
-        .catch((error) => {
-            console.log(error);
-            courseCode.value = error;
-        });
-    }
-
+    defineProps(['courseCode', 'courseName', 'courseStaff']);
 </script>
 
 <template>
     <div>
-        <br>
-        <button type="button" class="btn btn-outline-primary btn-lg" @click="getCourse">Get a course from the server</button>
-        <br>
-
         <div class="square">
-            <h3>{{ title }}</h3>
-            <br>
-            <h3>Wow so cute :3</h3>
             <!-- Course Data -->
-            <strong>Course Code: {{ courseCode }}<br></strong>
-            <strong>Name: {{ courseName }}<br></strong>
-            <strong>Staff: {{ courseStaff }}<br></strong>
+            <div class="rectangle">
+                <p class="rectangle-text">{{ courseCode }}<br></p>
+            </div>
+            <p>{{ courseName }}<br></p>
+
+            <div class="rectangle">
+                <!-- <p>Content</p> -->
+            </div>
         </div>
         
     </div>
 </template>
 
 <style>
+p {
+    color: white;
+    font-family: 'Courier New', Courier, monospace;
+    margin: 0;
+    font-size: 130%;
+}
 .square {
-    width: 250px;
-    height: 250px;
+    width: 200px;
+    height: 200px;
     background-color: black;
     text-align: center;
-    color: white;
-    padding-top: 10px;
-    font-size: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+}
+.rectangle {
+    width: 100%;
+    height: 15%;
+    background-color: grey;
+    display: flex;
     justify-content: center;
     align-items: center;
+}
+.rectangle-text {
+    font-size: 100%;
+    color: white;
+    font-weight: 200;
 }
 </style>
