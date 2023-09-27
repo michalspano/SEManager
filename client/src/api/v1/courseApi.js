e97206b1ed46c385fe9dcd12293353c59bbcc7af
Ionel Pop <ionelalexandru.pop@gmail.com>
2023-09-27 16:51:16 +0200

    #11 Fetch courses data
    Succesfully fetched data, now I just need to display it somehow

--- client/src/api/v1/courseApi.js
+++ client/src/api/v1/courseApi.js
@@ -5,21 +5,24 @@ const CourseApi = axios.create({
     baseURL: `${config.BASE_URL}:${config.PORT}/api/v${config.VERSION}/courses`
 })
 
-// export const getCourses = () => {
-//     CourseApi.get('/').then((response) => {
-//         console.log(response.data.courses);
-//         return response.data.courses
-//     }).catch((err) => {
-//         console.log(err)
-//     })
-// }
-
-export const getCourses = () => {
-    return CourseApi.get('/').then((response) => {
-        // console.log(response.data.courses);
+export const getCourses2 = () => {
+    CourseApi.get('/').then((response) => {
+        console.log(response.data.courses);
         return response.data.courses
     }).catch((err) => {
-        console.log(err);
-        throw err;
+        console.log(err)
     })
+}
+
+export const getCourses = async () => {
+    try {
+        console.log('courses 1');
+        const response = await CourseApi.get('/');
+        console.log(response);
+        console.log('courses 2');
+        return response.data.courses
+    } catch (err) {
+        console.log(err)
+        throw err
+    }
 }
--- client/src/views/CourseView.vue
+++ client/src/views/CourseView.vue
@@ -1,29 +1,59 @@
 <!-- Playground environment -->
 
-<script setup>
-import Semester from '@/components/Semester.vue'
+<script>
+// TODO: For now I'm passing pretty much all the information to the Year component
+// I need to fix that
 import Year from '@/components/Year.vue'
-import { ref } from 'vue'
+import Course from '@/components/Course.vue'
+import { onMounted, ref } from 'vue'
+import { getCourses } from '@/api/v1/courseApi';
 
 // I don't know how to get the data so for now I'm just doing dummy data lmao :((( I wanna die I don't understand javascript fr fr
 // Why is it called javascript? It doesn't have anything to do with java... I miss java :(
 
-const period1Courses = ref([
-    { courseCode: "DIT001", courseName: "Cringe 1", courseStaff: "ABC"},
-    { courseCode: "DIT002", courseName: "Cringe 2", courseStaff: "DEF"}
-])
+export default {
+    setup() {
+        const period1Courses = ref([
+            { courseCode: "DIT001", courseName: "Cringe 1", courseStaff: "ABC"},
+            { courseCode: "DIT002", courseName: "Cringe 2", courseStaff: "DEF"}
+        ]);
+        
+        const period2Courses = ref([
+            { courseCode: "DIT420", courseName: "Cringe Advanced", courseStaff: "ABC"},
+            { courseCode: "DIT421", courseName: "Cringe Expert", courseStaff: "DEF"}
+        ]);
 
-const period2Courses = ref([
-    { courseCode: "DIT420", courseName: "Cringe Advanced", courseStaff: "ABC"},
-    { courseCode: "DIT421", courseName: "Cringe Expert", courseStaff: "DEF"}
-])
+        const jsonResponse = ref(null);
+
+        onMounted(async () => {
+            jsonResponse.value = await getCourses();
+        })
+
+        return {
+            period1Courses,
+            period2Courses,
+            jsonResponse
+        }
+    },
+    components: {
+        Year,
+        Course
+    }
+}
+
 </script>
 
 
 <template>
     <div class="mainContainer">
         <h1>Main course view test</h1>
-
+        <h2>{{ jsonResponse }}</h2>
+        <Course
+            v-for="item in jsonResponse"
+            :courseName="item.courseName"
+            :courseCode="item.courseCode"
+            :courseStaff="item.courseStaff"
+        />
         <div class="programStructure">
             <Year yearTitle="Year 1" semester1Title="Semester 1" semester2Title="Semester 2" :period1Courses="period1Courses" :period2Courses="period2Courses"/>
             <Year yearTitle="Year 2" semester1Title="Semester 3" semester2Title="Semester 4" :period1Courses="period1Courses" :period2Courses="period2Courses"/>
