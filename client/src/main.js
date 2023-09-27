d3a93d6fb64ab06e00765b76ffc8894a69ed20ec
spano <spano@student.chalmers.se>
2023-09-27 21:13:16 +0200

    Disable `Boostrap.js`
    This commit removes the import of `Bootstrap.js` from `main.js`.
    Because we're using `Vue.js` and `Bootstrap.js` is not recommended
    to use with such a framework, we're removing it.
    
    This rationale is based on the documentation of `Bootstrap.js`:
    > https://getbootstrap.com/docs/5.3/getting-started/javascript/#usage-with-javascript-frameworks
    
    Furthermore, we're not using any of the features provided by `Bootstrap.js`
    and we're not planning to use them in the future (to not cause any potential
    conflicts with `Vue.js`).

--- client/src/main.js
+++ client/src/main.js
@@ -10,7 +10,3 @@ const app = createApp(App)
 app.use(router)
 
 app.mount('#app')
-
-// Ensure that HTML, CSS is loaded before additional
-// Bootstrap JS is handled.
-import 'bootstrap/dist/js/bootstrap.js'
