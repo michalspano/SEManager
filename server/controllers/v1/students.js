806c2cfe35b49ae604dcd83368bb343846242271
Ionel Pop <ionelalexandru.pop@gmail.com>
2023-09-29 03:34:21 +0200

    Add TODO in students.js

--- server/controllers/v1/students.js
+++ server/controllers/v1/students.js
@@ -13,6 +13,8 @@ const { fetchCourseIds, generateLinks } = require("../../utils/utils");
 
 const RESOURCE = "students";
 
+// TODO: Fix PUT and PATCH
+
 // Add a new student
 router.post('/', (req, res, next) => {
     fetchCourseIds(req.body.courses)
