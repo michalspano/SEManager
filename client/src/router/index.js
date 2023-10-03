e059e23fb3c4e716ebc6ce200baf5bb0de4d951d
spano <spano@student.chalmers.se>
2023-10-03 15:54:48 +0200

    #13 Update Router
    This commit makes changes to the Router of the client to accomodate
    the logic of the credential system. It also adds a new route for
    the login page. Additionally a mechanism, in an experimental stage,
    was added to redirect the user to the login page if they try to
    access a page that requires authentication without being logged in.

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '@/views/Home.vue'
+import Home from '@/views/Home.vue' // (merely for testing)
+import jwt_decode from "jwt-decode";
 
-// TODO: make the router handle unrecognized paths
-// and display an adequate error message
+// TODO: Redirect unrecognized routes to something like
+// 'invalid' and make a component for that. Similar to
+// 404 page not found.
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -12,16 +14,43 @@ const router = createRouter({
       component: Home
     },
     {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/LoginView.vue')
+    },
+    {
       path: '/courses',
       name: 'courses',
-      component: () => import('@/views/CourseView.vue')
+      // TODO: pass the prop based on the type of the user to CourseView
+      component: () => import('@/views/CourseView.vue'),
+      meta: { requiresAuth: true }
     },
     {
       path: '/courses/:id',
       name: 'course',
-      component: () => import('@/views/CourseInformationView.vue')
-    }
+      component: () => import('@/views/CourseInformationView.vue'),
+      meta: { requiresAuth: true }
+    },
   ]
 })
+
+// TODO: update this logic to recognize the user type too
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('token');
+  let decoded;
+  if (token) {
+    decoded = jwt_decode(token);
+  }
+
+  // TODO: inject this as a prop to CourseView
+  const userType = decoded?.type;
+
+  // If verification is required and no token is present, go to /login
+  if (to.meta.requiresAuth && !token) {
+    next('/login');
+  } else if (to.name === 'login' && token) {
+    next('/courses') // once logged in, go the CourseView
+  } else next();
+});
 
 export default router
