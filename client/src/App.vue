b6cb05035acd8c9fb40d8bb45b78490102f1d705
spano <spano@student.chalmers.se>
2023-10-05 23:18:08 +0200

    Add header, footer
    This commit adds the Header and the Footer as separate Vue components.
    Note: they are in their initial state and will be updated later.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,17 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
 </script>
 
 <!-- the body of the component -->
 <template>
+  <Header/>
   <nav>
     <!-- Render the content of the current page view -->
     <RouterView/>
   </nav>
+  <Footer/>
 </template>
 
 <style scoped>
