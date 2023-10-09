42da034e739622d3beb19f00e3c1b54ab9c38ab0
Erik <elindstr@student.chalmers.se>
2023-10-09 17:16:30 +0200

    Add 'employeeApi.js'
    This commit adds the 'employeeApi.js' file to define methods for fetching employee data in the front-end.

---
+++ client/src/api/v1/employeeApi.js
@@ -0,0 +1,44 @@
+/**
+ * api/v1/employeeApi.js
+ * 
+ * @description :: Axios configuration for the Employees.
+ * @version     :: 1.0
+ */
+
+import axios from "axios";
+import * as config from '@/api/v1/apiConfig'
+
+/**
+ * EmployeeApi is an Axios instance which provides the baseURL for all the HTTP requests.
+ */
+const EmployeeApi = axios.create({
+    baseURL: `${config.BASE_URL}:${config.PORT}/api/v${config.VERSION}/employees`
+})
+
+/**
+ * Function to return all the employees.
+ * @returns {Promise} Promise object represents the list of employees.
+ */
+export const getEmployees = async () => {
+    try {
+        const response = await EmployeeApi.get('/');
+        return response.data.employees
+    } catch (err) {
+        console.log(err)
+        throw err
+    }
+}
+
+/**
+ * Function to return a specific employee.
+ * @returns {Promise} Promise object represents the employee.
+ */
+export const getEmployee = async (employeeEmailAddress) => {
+    try {
+        const response = await EmployeeApi.get('/' + employeeEmailAddress);
+        return response.data.employee
+    } catch (err) {
+        console.log(err)
+        throw err
+    }
+}
