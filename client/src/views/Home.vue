4c5eae144e9bf5e57796f24c0ee895083dc6001b
spano <spano@student.chalmers.se>
2023-10-10 00:33:52 +0200

    Draft a provisional update to `Home.vue`
    This commit is a draft of the changes to the `Home.vue` file. It is
    intended to be used as a reference for the final commit.

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,20 +1,52 @@
+<!-- TODO: fix formatting -->
 <template>
-    <!-- A simple simulation of the previous template -->
     <div class="container py-4">
-        <h1 class="display-2">DIT342 Group &#45; 15</h1>
         <div class="p-5 mb-4 bg-body-tertiary rounded-3">
-            <button type="button" class="btn btn-outline-primary btn-lg" @click="updateMessage">Get response</button>
-            <p>The server says:<br/>{{ message }}</p>
+            <p class="fs-4"><i>&#8208; for the "real" SEM experience</i></p>
+            <div class="row">
+                <div class="col-md-6">
+                    <p>
+                        Welcome to our innovative solution tailored for <b>SEM</b> (Software Engineering and Management)
+                        students!
+                        Our mission is to enhance your academic journey by offering a comprehensive tool that simplifies the
+                        <b>visualization</b> of your program's structure.
+                    </p>
+                    <p>
+                        With our platform, you can effortlessly plan your
+                        studies, monitor your progress, and gain insights into course prerequisites. Experience a <b>new
+                            level
+                            of control</b> and efficiency in your academic pursuits
+                        with <strong>SEManager</strong>.
+                    </p>
+                    <p>
+                        Your academic success starts here!
+                    </p>
+                </div>
+                <div class="col-md-6">
+                    <!-- TODO: update with the modified logo -->
+                    <img src="@/assets/logo.gif" width=300px alt="SEManager logo" class="img-fluid rounded logotype" />
+                </div>
+            </div>
         </div>
-        <!-- FIXME: this is just an example placement and will be updated -->
-        <ApiInfo style="width: 30em;"/>
+        <div class="p-3">
+            <div class="row">
+                <div class="col-md-6 mt-3 order-md-1">
+                    <ApiInfo />
+                </div>
+                <div class="col-md-6 order-md-2 d-flex justify-content-center align-items-center">
+                    <router-link to="/courses" class="btn btn-primary btn-lg router-link-class">
+                        Explore Courses
+                    </router-link>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import { ref } from 'vue';
 import { getApi } from '@/api/v1/Api';
-import ApiInfo from '../components/ApiInfo.vue';
+import ApiInfo from '@/components/ApiInfo.vue';
 
 export default {
     name: 'Home',
@@ -32,3 +64,27 @@ export default {
 };
 </script>
 
+<style scoped>
+/* make the button appear bigger */
+.btn-lg {
+    padding: 1rem 2rem;
+    font-size: 1.5rem;
+    font-weight: 700;
+}
+
+@media (max-width: 768px) {
+    .btn-lg {
+        font-size: 1.2rem;
+        padding: 0.75rem 1rem;
+    }
+
+    .router-link-class {
+        margin-top: 30px;
+    }
+
+    .logotype {
+        width: 350px;
+        height: auto;
+    }
+}
+</style>
