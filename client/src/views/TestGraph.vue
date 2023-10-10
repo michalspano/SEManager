4e2f6ee7a1b71dadcef5d2d9822acbddcf2cddc5
Ionel Pop <ionelalexandru.pop@gmail.com>
2023-10-10 02:09:28 +0200

    Update TestGraph.vue

--- client/src/views/TestGraph.vue
+++ client/src/views/TestGraph.vue
@@ -1,5 +1,4 @@
 <template>
-
     <div class="container-fluid text-center">
         <div class="row">
             <h1>Node Test</h1>
@@ -33,155 +32,144 @@
 
 </template>
 
-<script>
-import { ref, onMounted } from 'vue';
+<script setup>
+import { ref, onMounted, computed } from 'vue';
 import { getCoursesGraph } from '@/api/v1/courseApi';
-import CourseNode from '@/components/CourseNode.vue';
 import YearContainer from '@/components/YearContainer.vue'
 
-export default {
-    name: 'Home',
-    setup() {
-        const graphData = ref(null);
-        
-        onMounted(async () => {
-            graphData.value = await getCoursesGraph();
-        });
+const graphData = ref(null);
 
-        const testStatus = (courseCode, status) => {
+const testStatus = (courseCode, status) => {
             updateCourseStatus(courseCode, status);
             updateCoursesCompletionStatus();
+        };
+
+onMounted(async () => {
+    graphData.value = await getCoursesGraph();
+});
+
+const updateCourseStatus = (courseCode, status) => {
+    for (var i of graphData.value.keys())
+    {
+        if(i['courseCode'] === courseCode)
+        {
+            i['courseStatus'] = status;
+        }
+    }
+}
+
+const getCourseStatus = (courseCode) => {
+    var targetCourse = null;
+    for(var i of graphData.value.keys())
+    {
+        if(i['courseCode'] === courseCode)
+        {
+            targetCourse = i;
+            break;
+        }
+    }
+    
+    return targetCourse['courseStatus'];
+}
+
+const updateCoursesCompletionStatus = () => {
+     // Go through all the courses and see if the dependencies are fulfilled
+    // If the dependencies have both status 2, then unlock the course (set status 1)
+    // If not, set status 0 (locked)
+    // console.log("Update courses");
+    for (var i of graphData.value.keys())
+    {
+        if (getCourseStatus(i['courseCode']) === 2) {
+            // console.log(i['courseCode'] + ' is completed :)!');
+            continue;
+        }
+        let dependencies = graphData.value.get(i);
+        let numberOfDependencies = dependencies.length;
+        // console.log(i['courseCode'] + ' has ' + numberOfDependencies + ' dependencies: ');
+
+        const totalPoints = numberOfDependencies * 2;
+
+        let sumOfDependenciesStatus = 0;
+        for (var j of dependencies)
+        {
+            sumOfDependenciesStatus += getCourseStatus(j);
+            // console.log(j + ' with status ' + getCourseStatus(j));
         }
 
-        const updateCourseStatus = (courseCode, status) => {
-            for (var i of graphData.value.keys())
-            {
-                if(i['courseCode'] === courseCode)
-                {
-                    i['courseStatus'] = status;
-                }
-            }
+        // console.log('Sum of dependencies: ' + sumOfDependenciesStatus);
+
+        if(sumOfDependenciesStatus === totalPoints)
+        {
+            // console.log("Unlocked!");
+            i['courseStatus'] = 1;
+        }
+        else {
+            // console.log("Locked :(");
+            i['courseStatus'] = 0;
         }
 
-        const getCourseStatus = (courseCode) => {
-            var targetCourse = null;
-            for(var i of graphData.value.keys())
-            {
-                if(i['courseCode'] === courseCode)
-                {
-                    targetCourse = i;
-                    break;
-                }
-            }
+        console.log('\n');
+    }
+}
 
-            return targetCourse['courseStatus'];
-        }
+/*
+    * TODO: There is some repetition going on here but there's no much to do
+    * I would like to make this a parameter but  vue doesn't allow it
+*/
 
-        const updateCoursesCompletionStatus = () => {
-            // Go through all the courses and see if the dependencies are fulfilled
-            // If the dependencies have both status 2, then unlock the course (set status 1)
-            // If not, set status 0 (locked)
-            // console.log("Update courses");
-            for (var i of graphData.value.keys())
-            {
-                if (getCourseStatus(i['courseCode']) === 2) {
-                    // console.log(i['courseCode'] + ' is completed :)!');
-                    continue;
-                }
-                let dependencies = graphData.value.get(i);
-                let numberOfDependencies = dependencies.length;
-                // console.log(i['courseCode'] + ' has ' + numberOfDependencies + ' dependencies: ');
-
-                const totalPoints = numberOfDependencies * 2;
-
-                let sumOfDependenciesStatus = 0;
-                for (var j of dependencies)
-                {
-                    sumOfDependenciesStatus += getCourseStatus(j);
-                    // console.log(j + ' with status ' + getCourseStatus(j));
-                }
-
-                // console.log('Sum of dependencies: ' + sumOfDependenciesStatus);
-
-                if(sumOfDependenciesStatus === totalPoints)
-                {
-                    // console.log("Unlocked!");
-                    i['courseStatus'] = 1;
-                }
-                else {
-                    // console.log("Locked :(");
-                    i['courseStatus'] = 0;
-                }
-
-                console.log('\n');
-            }
-        }
-
-        return { graphData, testStatus, getCourseStatus };
-    },
-    components: {
-        CourseNode,
-        YearContainer
-    },
-    computed: {
-        firstYearCourses() {
+const firstYearCourses  = computed(() => {
             
-            if (!this.graphData) {
-                return null;
-            }
+    if (!graphData.value) {
+        return null;
+    }
+    
+    // Convert the Map to an array of key-value pairs
+    const graphArray = Array.from(graphData.value);
+
+    let year = 1;
+
+    let firstTerm = (year * 2) - 1;
+    let secondTerm = year * 2;
+
+
+    return graphArray.filter(([key, _]) => 
+        key.courseTerm === firstTerm || key.courseTerm === secondTerm
+    );
+});
+
+const secondYearCourses = computed(() => {
             
-            // Convert the Map to an array of key-value pairs
-            const graphArray = Array.from(this.graphData);
+    if (!graphData.value) {
+        return null;
+    }
 
-            // I would like to make this a parameter but vue doesn't allow it
-            // TODO: There is some repetition going on here but there's no much to do
-            // Otherwise it looks too bloated
-            let year = 1;
+    const graphArray = Array.from(graphData.value);
+    
+    let year = 2;
 
-            let firstTerm = (year * 2) - 1;
-            let secondTerm = year * 2;
+    let firstTerm = year * 2 - 1;
+    let secondTerm = year * 2;
 
+    return graphArray.filter(([key, _]) => 
+        key.courseTerm === firstTerm || key.courseTerm === secondTerm
+    );
+});
 
-            return graphArray.filter(([key, _]) => 
-                key.courseTerm === firstTerm || key.courseTerm === secondTerm
-            );
-        },
-        secondYearCourses() {
-            
-            if (!this.graphData) {
-                return null;
-            }
+const thirdYearCourses = computed(() => {
 
-            let year = 2;
+    if (!graphData.value) {
+        return null;
+    }
 
-            let firstTerm = year * 2 - 1;
-            let secondTerm = year * 2;
-            
-            // Convert the Map to an array of key-value pairs
-            const graphArray = Array.from(this.graphData);
+    const graphArray = Array.from(graphData.value);
+    
+    let year = 3;
 
-            return graphArray.filter(([key, _]) => 
-                key.courseTerm === firstTerm || key.courseTerm === secondTerm
-            );
-        },
-        thirdYearCourses() {
-            
-            if (!this.graphData) {
-                return null;
-            }
+    let firstTerm = year * 2 - 1;
+    let secondTerm = year * 2;
 
-            let year = 3;
-
-            let firstTerm = year * 2 - 1;
-            let secondTerm = year * 2;
-            
-            // Convert the Map to an array of key-value pairs
-            const graphArray = Array.from(this.graphData);
-
-            return graphArray.filter(([key, _]) => 
-                key.courseTerm === firstTerm || key.courseTerm === secondTerm
-            );
-        }
-    },
-};
+    return graphArray.filter(([key, _]) => 
+        key.courseTerm === firstTerm || key.courseTerm === secondTerm
+    );
+});
 </script>
