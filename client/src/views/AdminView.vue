533a482b85d5342e147e11e3c66a23b63985e1bc
spano <spano@student.chalmers.se>
2023-10-12 21:40:48 +0200

    Add view for admin components
    This commit adds a view for the admin components. This view will be
    used to add and delete users, employees, and courses. The view can be
    accessed by navigating to /admin (it is protected by the admin guard).

---
+++ client/src/views/AdminView.vue
@@ -0,0 +1,28 @@
+<script setup>
+import AdminDeleteSelector from '@/components/AdminDeleteSelector.vue';
+import AdminActionSelector from '@/components/AdminActionSelector.vue';
+</script>
+
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="container py-3">
+                    <div class="p-4 bg-body-tertiary rounded-4 subtle-shadow">
+                        <p class="fs-1 fw-bolder mx-2">Add/Update</p>
+                        <AdminActionSelector />
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="container py-3">
+                    <div class="p-4 bg-body-tertiary rounded-4 subtle-shadow">
+                        <p class="fs-1 fw-bolder mx-2">Delete</p>
+                        <AdminDeleteSelector/>
+                    </div>
+                </div>
+                <h1>TODO: ADD LAST CONTAINER</h1>
+            </div>
+        </div>
+    </div>
+</template>
