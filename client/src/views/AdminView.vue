f6b551baae6595c87f88a3e0b9b27df0448b260e
Erik <elindstr@student.chalmers.se>
2023-10-17 14:53:18 +0200

    Restructure 'AdminView.vue'
    This commit adds a div to fix styling issues in 'AdminView.vue'.

--- client/src/views/AdminView.vue
+++ client/src/views/AdminView.vue
@@ -5,26 +5,28 @@ import AdminActionSelector from '@/components/AdminActionSelector.vue';
 </script>
 
 <template>
-    <div class="container-fluid py-4">
-        <router-link to="/courses">
-            <img src="@/assets/back-icon.png" alt="Back to courses" id="nav-back-icon" width="75" height="75"
-                class="d-inline-block">
-        </router-link>
-    </div>
-    <div class="container">
-        <div class="row">
-            <!-- Note: herein, the layout changes at large, so that the text size is not too small -->
-            <div class="col-lg-6">
-                <div class="container py-3">
-                    <AdminActionSelector />
+    <div>
+        <div class="container-fluid py-4">
+            <router-link to="/courses">
+                <img src="@/assets/back-icon.png" alt="Back to courses" id="nav-back-icon" width="75" height="75"
+                    class="d-inline-block">
+            </router-link>
+        </div>
+        <div class="container">
+            <div class="row">
+                <!-- Note: herein, the layout changes at large, so that the text size is not too small -->
+                <div class="col-lg-6">
+                    <div class="container py-3">
+                        <AdminActionSelector />
+                    </div>
                 </div>
-            </div>
-            <div class="col-lg-6">
-                <div class="container py-3">
-                    <AdminDeleteSelector />
-                </div>
-                <div class="container py-3">
-                    <AdminManual />
+                <div class="col-lg-6">
+                    <div class="container py-3">
+                        <AdminDeleteSelector />
+                    </div>
+                    <div class="container py-3">
+                        <AdminManual />
+                    </div>
                 </div>
             </div>
         </div>
