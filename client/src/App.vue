8b1623f9a2e34d3491db722c9a7714346837b966
Erik <elindstr@student.chalmers.se>
2023-10-17 14:42:48 +0200

    Restructure 'App.vue'
    This commit restructures 'App.vue' to fix styling errors.

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,18 +6,20 @@ import Footer from '@/components/Footer.vue'
 
 <!-- the body of the component -->
 <template>
-  <Header/>
-  <nav id="router-view">
-    <!-- Render the content of the current page view -->
-    <RouterView/>
-  </nav>
-  <Footer/>
+  <div>
+    <Header></Header>
+    <nav id="router-view">
+      <!-- Render the content of the current page view -->
+      <RouterView/>
+    </nav>
+    <Footer></Footer>
+  </div>
 </template>
 
 <style scoped>
 #router-view {
   min-height: 100vh;
-  background-color: var(--primary-color); /* Make bg color consistent with the global
-                                            value from main.css */
+  background-color: var(--primary-color);
+  /* Make bg color consistent with the global value from main.css */                         
 }
 </style>
